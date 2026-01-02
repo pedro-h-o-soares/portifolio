8ca3cc926ce72f004c8e4c74e70b3e070b00af6d
pedro-h-o-soares <pedrosoares77744@gmail.com>
2026-01-02 00:02:10 -0300

    fix: adjust CSP for scripts execution

--- js/app.js
+++ js/app.js
@@ -45,6 +45,12 @@ function updateCurrentJob() {
   $('.current-job-start').html(`Since ${moment(date).format('MMM/YYYY')}`)
 }
 
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "ut5aer4tam");
+
 $(document).ready(function () {
   const anchor = window.location.hash;
   $(anchor).addClass('anchor');
